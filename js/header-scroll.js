411f5bc0aa7302d1a013099ff765b0abb74a66d2
Leandro Falcon <cronosteo1034@gmail.com>
2022-09-25 19:00:52 -0300

    cambios background

--- js/header-scroll.js
+++ js/header-scroll.js
@@ -1,10 +1,25 @@
 const header = document.querySelector(".menu-nav");
+const scrollUp = "scroll-up";
+const scrollDown = "scroll-down";
+let lastScroll = 0;
+const body = document.body;
+
 
 window.addEventListener("scroll", () => {
-  const scrollPos = window.scrollY;
-  if (scrollPos > 10) {
-    header.classList.add("scrolled");
-  } else {
-    header.classList.remove("scrolled");
+  const currentScroll = window.pageYOffset;
+  if (currentScroll ==0) {
+    header.classList.remove(scrollUp);
+    return;
   }
+   
+  if (currentScroll > lastScroll && !header.classList.contains(scrollDown)) {
+    // down
+    header.classList.remove(scrollUp);
+    header.classList.add(scrollDown);
+  } else if (currentScroll < lastScroll && header.classList.contains(scrollDown)) {
+    // up
+    header.classList.remove(scrollDown);
+    header.classList.add(scrollUp);
+  }
+  lastScroll = currentScroll+10;
 });
